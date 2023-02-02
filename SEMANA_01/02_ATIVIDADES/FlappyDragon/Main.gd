f069f722e9729c2a01cdae9c753cf0611447fe3c
ahnina <ana.marques@sou.inteli.edu.br>
2023-02-02 11:31:33 -0300

    Revert "flappydragon.atividade"
    This reverts commit 8c45d66a31ea70846c1993b3e4ae2cc6366bf943.

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,9 @@
 extends Node # instancia a classe Node2D
 
-var status = 0
+var status = 1
 var vscore = 0
-var x = 5
-var y = 5 
+var x = 1.5 
+var y = 1.5 
 
 # executa essa função ao carregar o jogo
 func _ready():
